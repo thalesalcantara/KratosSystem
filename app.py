a95256d3feed8d7704c780d07e68809fe6a5435e
Coopex Entregas <thalesalcantara88@gmail.com>
2025-08-11 18:40:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -666,9 +666,9 @@ def estab_excluir_lancamento(id):
         flash('Você não tem permissão para excluir este lançamento.', 'danger')
         return redirect(url_for('painel_estabelecimento'))
 
-    # janela de 1 hora
-    if datetime.utcnow() - l.data > timedelta(hours=1):
-        flash('Exclusão permitida somente até 1 hora após a criação.', 'warning')
+    # janela de 5 hora
+    if datetime.utcnow() - l.data > timedelta(hours=5):
+        flash('Exclusão permitida somente até 5 hora após a criação.', 'warning')
         return redirect(url_for('painel_estabelecimento'))
 
     cooperado = Cooperado.query.get(l.cooperado_id)
